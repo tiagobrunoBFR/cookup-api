08dce343bb689915613bec08c2d3f56256828e9d
tiago <tiagobrunobfr@gmail.com>
2020-07-15 22:26:16 -0300

    add config jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,24 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     use Notifiable;
 
     /**
