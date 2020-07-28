ea2a0f6df9638b13b0010ea56b12369e1f906224
tiago <tiagobrunobfr@gmail.com>
2020-07-28 12:42:17 -0300

    refactor register ingredient

--- app/Http/Controllers/Api/Admin/IngredientController.php
+++ app/Http/Controllers/Api/Admin/IngredientController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\IngredientRequest;
-use App\Models\File;
 use App\Models\Ingredient;
+use App\Services\File\Upload;
 use Illuminate\Http\Request;
 
 class IngredientController extends Controller
 {
+
+
     /**
      * Display a listing of the resource.
      *
@@ -23,37 +25,28 @@ class IngredientController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(IngredientRequest $request)
     {
 
-        $image = $request->file('image');
+        $path = 'ingredients';
 
-        $path = $image->store('ingredients', 'public');
+        $image = new Upload($path, $request->file('image'));
 
+        $ingredient = Ingredient::create([
+            'name' => $request->name,
+            'image_id' => $image()->id,
+        ]);
 
-     $image_id =  File::create([
-            'mime' => $image->getClientMimeType(),
-            'path' => $path,
-            'name' => $image->getClientOriginalName(),
-            'size' => $image->getSize()
-        ])->id;
-
-
-     $ingredient = Ingredient::create([
-           'name' => $request->name,
-           'image_id' => $image_id,
-       ]);
-
-       return response()->json($ingredient, 201);
+        return response()->json($ingredient, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -64,8 +57,8 @@ class IngredientController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -76,7 +69,7 @@ class IngredientController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
---
+++ app/Services/File/Upload.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Services\File;
+
+
+use App\Models\File;
+use Illuminate\Http\Request;
+
+class Upload
+{
+
+    private $path;
+    private $file;
+    private $disk;
+
+    public function __construct($path, $file, $disk = 'public')
+    {
+        $this->path = $path;
+        $this->file = $file;
+        $this->disk = $disk;
+    }
+
+
+    public function __invoke()
+    {
+        return $this->upload();
+    }
+
+    public function upload()
+    {
+        $path = $this->file->store($this->path, $this->disk);
+
+        return $this->store($path);
+    }
+
+    public function store($path)
+    {
+        return File::create([
+            'mime' => $this->file->getClientMimeType(),
+            'path' => $path,
+            'name' => $this->file->getClientOriginalName(),
+            'size' => $this->file->getSize()
+        ]);
+    }
+
+
+}
